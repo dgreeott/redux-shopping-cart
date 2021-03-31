f341d5e0cb73a7567b618dd0aa877d69a1a8c983
dgreeott <48890665+dgreeott@users.noreply.github.com>
2021-03-31 14:37:26 -0600

    Starting Project

---
+++ src/components/Navbar/ListItems.js
@@ -0,0 +1,19 @@
+import { FiShoppingCart } from "react-icons/fi";
+
+export const ListItems = [
+    {
+      title: "Shop",
+      url: "/",
+      cName: "nav-links",
+    },
+    {
+      title: "My Cart",
+      url: "/cart",
+      cName: "nav-links",
+    },
+    {
+      title: <FiShoppingCart />,
+      url: "/cart",
+      cName: "nav-links",
+    },
+  ];
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+import { ListItems } from "./ListItems.js";
+
+class Navbar extends Component {
+  state = { clicked: false };
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+
+  render() {
+    return (
+      <nav className="navbarItems">
+      <Link to="/" className="brand-logo">Shopping</Link>
+        <div className="menu-icon" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+          {ListItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <Link className={item.cName} to={item.url}>
+                  {item.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
